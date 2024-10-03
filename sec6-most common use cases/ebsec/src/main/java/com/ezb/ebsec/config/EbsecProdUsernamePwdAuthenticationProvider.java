a093f432fef1d2eb712b4f781b5055ecf5ed2b07
Pratik <pratikwankhede008@gmail.com>
2024-10-03 20:10:18 +0530

    most common use cases

---
+++ sec6-most common use cases/ebsec/src/main/java/com/ezb/ebsec/config/EbsecProdUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.ezb.ebsec.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+public class EbsecProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+    
+    private final UserDetailsService userDetailsService;
+    
+    private final PasswordEncoder passwordEncoder;
+
+    public EbsecProdUsernamePwdAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if(passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+        }
+        else {
+            throw new BadCredentialsException("Invalid password");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
